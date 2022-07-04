d82fb392d76ae9d0fd087576a018885dc5959a41
wjdgh4058 <ljhmoa@gmail.com>
2022-07-04 18:56:41 +0900

    test4

--- bigdata/data_crawling.py
+++ bigdata/data_crawling.py
@@ -4,4 +4,12 @@
 
 # 크롤링 : 웹 스크레이핑에 필요한 여러개의 페이지들을 오고가며 수집
 
-#
+# 사용할 Python Library
+
+# pandas : Data preprocessing/ Save & Load (데이터 전처리)
+
+# Selenium : Crawling + (chrome driver)
+
+# Request : 웹 수집
+
+#Urllib : image Data + scrap & save
