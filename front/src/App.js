7c1c77f14487899f7f51114f6671cd287af576ee
leeuihyun <meta212121@naver.com>
2022-07-05 15:26:37 +0900

    App.js Route

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LogIn from "./pages/LogIn";
+import Map from "./pages/Map";
 
 const App = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<LogIn />}></Route>
+            <Route path="login" element={<LogIn />}></Route>
+            <Route path="map" element={<Map />}></Route>
         </Routes>
     );
 };
