bf72fb3759fc8e1a15080e817fbf01ce8687ec2e
leeuihyun <meta212121@naver.com>
2022-07-06 20:21:50 +0900

    route +

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LogIn from "./pages/LogIn";
 import Map from "./pages/Map";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
             <Route path="/" element={<Home />}></Route>
             <Route path="login" element={<LogIn />}></Route>
             <Route path="map" element={<Map />}></Route>
+            <Route path="signup" element={<SignUp />}></Route>
         </Routes>
     );
 };
