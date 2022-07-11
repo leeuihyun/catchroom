b75f8dba33b8238c16deb55e24c39b9c84f7b84e
지상일 <88275989+sangilji@users.noreply.github.com>
2022-07-11 18:33:22 +0900

    join logic

--- backend/src/main/java/catchroom/backend/controller/MemberForm.java
+++ backend/src/main/java/catchroom/backend/controller/MemberForm.java
@@ -7,9 +7,10 @@ import lombok.Setter;
 @Getter @Setter
 public class MemberForm {
 
-    private String name;
+
     private String email;
     private String password;
+    private String name;
     private String number;
 
     private String city;
