63c39acb36cb9b0c4667fb968e5053c981f3de39
leeuihyun <meta212121@naver.com>
2022-07-13 16:51:56 +0900

    메인 페이지 레이아웃 컴포넌트 수정

--- front/src/components/HomeLayout.jsx
+++ front/src/components/HomeLayout.jsx
@@ -82,23 +82,25 @@ const HomeLayout = () => {
         justifyContent: "center",
         alignItems: "center",
     };
+
     return (
         <Box>
-            <Header />
             <FullPage>
                 <FullPageSections>
-                    <FullpageSection style={sectionStyle}>
-                        <h1>당신의 방을 찾아보세요</h1>
-
-                        <Input>
-                            <input
-                                type="text"
-                                placeholder="지역 혹은 학교명을 입력해주세요"
-                            />
-                            <SearchOutLineIcon>
-                                <SearchOutlined />
-                            </SearchOutLineIcon>
-                        </Input>
+                    <FullpageSection>
+                        <Header></Header>
+                        <div style={sectionStyle}>
+                            <h1>당신의 방을 찾아보세요</h1>
+                            <Input>
+                                <input
+                                    type="text"
+                                    placeholder="지역 혹은 학교명을 입력해주세요"
+                                />
+                                <SearchOutLineIcon>
+                                    <SearchOutlined />
+                                </SearchOutLineIcon>
+                            </Input>
+                        </div>
                     </FullpageSection>
                     <FullpageSection style={sectionStyle}>
                         <h1>screen 2</h1>
