0f04d64a1605077e88bfa2e645caaecdf78cfe83
leeuihyun <meta212121@naver.com>
2022-08-01 18:12:56 +0900

    렌더링 두번 되는 것을 막기 위해 stricMode -

--- front/src/index.js
+++ front/src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 import store from "./store/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
