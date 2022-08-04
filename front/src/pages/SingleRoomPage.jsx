fe62077ba38f6d6c75632b7e6bb38fdf540cc4c1
leeuihyun <meta212121@naver.com>
2022-08-04 15:49:06 +0900

    싱글 페이지

---
+++ front/src/pages/SingleRoomPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SingleRoomComponent from "../components/SingleRoomComponent";
+
+const SingleRoomPage = () => {
+    return (
+        <>
+            <SingleRoomComponent />
+        </>
+    );
+};
+
+export default SingleRoomPage;
