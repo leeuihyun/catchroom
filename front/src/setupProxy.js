c60514f4ceb368272b9df40680ef641a6fd5ceab
leeuihyun <meta212121@naver.com>
2022-08-04 20:46:08 +0900

    프록시 주소 추가

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
     app.use(
-        createProxyMiddleware(["/members", "/me"], {
+        createProxyMiddleware(["/members", "/me", "/rooms"], {
             target: "http://52.79.85.130",
             changeOrigin: true,
             ws: true,
