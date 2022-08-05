9258215228a1b4bc1933c540d00ff1fed7677e6b
leeuihyun <meta212121@naver.com>
2022-08-05 19:15:45 +0900

    저장소 모달 삭제

--- front/src/store/store.jsx
+++ front/src/store/store.jsx
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../reducers/userSlice";
-import modalSlice from "../reducers/modalSlice";
 import roomSlice from "../reducers/roomSlice";
 
 import axios from "axios";
@@ -10,7 +9,6 @@ axios.defaults.withCredentials = true;
 const store = configureStore({
     reducer: {
         user: userSlice,
-        modal: modalSlice,
         room: roomSlice,
     },
 });
