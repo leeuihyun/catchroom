fcbae57e4e33222c7cd8e0e7bf2fea70a1dcb4d8
leeuihyun <meta212121@naver.com>
2022-08-08 16:03:49 +0900

    useLocation을 이용해서 페이지 이동하며 props로 데이터 전달하기

--- front/src/components/SearchMapLayout.jsx
+++ front/src/components/SearchMapLayout.jsx
@@ -53,7 +53,7 @@ const MapContainer = styled.div`
 const SearchMapLayout = () => {
     const dispatch = useDispatch();
     const COOKIE = localStorage.getItem("cookie");
-    const { room, searchCount, now } = useSelector((state) => state.room);
+    const { room, searchCount } = useSelector((state) => state.room);
     const pageNumbers = Math.ceil(searchCount / 20);
     const [page, setPage] = useState(1);
     const offset = (page - 1) * 20;
@@ -142,7 +142,7 @@ const SearchMapLayout = () => {
                     <Card
                         key={item.id}
                         index={i}
-                        data={item.roomInfo.주소}
+                        data={item.roomInfo}
                         onClick={onClickCard}
                     >
                         <div>
