d9843b82bb0b67202f6b663c2342e43e0c4706ee
leeuihyun <meta212121@naver.com>
2022-08-16 22:00:37 +0900

    찜한 목록 컴포넌트

--- front/src/components/MyPageComponent.jsx
+++ front/src/components/MyPageComponent.jsx
@@ -1,9 +1,7 @@
-import React from "react";
 import Header from "./Header";
 import styled from "styled-components";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
+import WishCard from "./WishCard";
 const MyPageComponent = () => {
     const { studentUser } = useSelector((state) => state.user);
     const wishRooms = studentUser?.wishRooms;
@@ -15,10 +13,7 @@ const MyPageComponent = () => {
                 <WishText>관심 목록</WishText>
                 <WishList>
                     {wishRooms?.map((room) => (
-                        <WishCard>
-                            <div>대학교 : {room.roomInfo.대학교}</div>
-                            <div>주소 : {room.roomInfo.주소}</div>
-                        </WishCard>
+                        <WishCard room={room} />
                     ))}
                 </WishList>
             </Main>
@@ -31,16 +26,7 @@ const WishText = styled.div``;
 const WishList = styled.div`
     display: flex;
     flex-wrap: wrap;
-    background-color: black;
-    width: 100vw;
-    height: 100vh;
-    color: white;
-`; //방 목록 띄울 곳
-const WishCard = styled.div`
     background-color: white;
-    width: 235px;
-    height: 240px;
-    color: black;
-    margin: 26px;
-`; //방 하나하나 목록
+`; //방 목록 띄울 곳
+
 export default MyPageComponent;
