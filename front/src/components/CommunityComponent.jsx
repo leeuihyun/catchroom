b421a78dd9765b64ec2a1ec82d4ff896c56d0e22
leeuihyun <meta212121@naver.com>
2022-09-02 10:43:30 +0900

    폼 설정

--- front/src/components/CommunityComponent.jsx
+++ front/src/components/CommunityComponent.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Header from "./Header";
 import styled from "styled-components";
 import Footer from "./Footer";
+import { Avatar, List } from "antd";
 
 function CommunityComponent() {
     return (
         <>
             <Header color="black" />
             <CommunityBox>
-                <CommunityForm>hi</CommunityForm>
+                <CommunityForm>
+                    <CommunityTitle>커뮤니티</CommunityTitle>
+                </CommunityForm>
                 <Footer></Footer>
             </CommunityBox>
         </>
@@ -28,8 +31,15 @@ const CommunityBox = styled.div`
 
 const CommunityForm = styled.div`
     width: 80%;
-    height: 80%;
+    height: 90%;
     background-color: white;
     color: black;
+    display: flex;
+    flex-direction: column;
+`;
+
+const CommunityTitle = styled.div`
+    font-weight: bold;
+    font-size: 30px;
 `;
 export default CommunityComponent;
